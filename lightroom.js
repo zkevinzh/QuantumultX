6a21bb02c58e55f6d08a3455803c9761f5d62322
zkevinzh <46732723+zkevinzh@users.noreply.github.com>
2025-10-09 09:15:02 +0800

    Update lightroom.js

--- lightroom.js
+++ lightroom.js
@@ -1,11 +1,6 @@
 body = $response.body.replace(/while.{7}\n/, "");
 let obj = JSON.parse(body);
 obj.entitlement.status="subscriber";
-obj.current_subs={
-      "product_id": "Photography",
-      "store": "adobe",
-      "purchase_date": "2025-10-01T16:32:10.254954Z",
-    };
 obj.entitlement.storage={
       "used": 0,
       "limit": 1154487209165,
