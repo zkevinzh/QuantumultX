9550fb3569cc5a1a55d7c72ca56275ba4d5e362e
zkevinzh <46732723+zkevinzh@users.noreply.github.com>
2025-10-17 22:01:05 +0800

    Update lightroom.js

--- lightroom.js
+++ lightroom.js
@@ -1,30 +1,34 @@
-let body = $response.body.replace(/while.{7}\n/, "");
+body = $response.body.replace(/while.{7}\n/, "");
 let obj = JSON.parse(body);
-
-// Set subscriber status
-obj.entitlement = {
-  status: "subscriber",
-  current_subs: {
-    product_id: "lightroom",
-    store: "adobe",
-    purchase_date":"2025-10-07T12:41:39.856692Z"
-  },
-  storage: {
-    used: 0,
-    limit: 112742891520,
-    display_limit: 107374182400,
-    warn: 85899345920
-  }
-};
-
-// Add missing config field that signals premium
-obj.config = obj.config || {};
-obj.config.one_quota = true;
-
-// Optionally patch payload if missing
-obj.payload = obj.payload || {};
-obj.payload.universal = obj.payload.universal || {};
-obj.payload.universal.communication_preference = "sv-EG";
-
-body = JSON.stringify(obj);
-$done({ body });
+obj.entitlement.status="subscriber";
+obj.current_subs={
+      "product_id": "lightroom",
+      "store": "adobe",
+      "purchase_date":"2025-10-07T12:41:39.856692Z",
+      "sao": {
+        "inpkg_CCES": "0",
+        "inpkg_CCLE": "1",
+        "inpkg_CCSN": "0",
+        "inpkg_CCSV": "0",
+        "inpkg_LCCC": "0",
+        "inpkg_LPES": "0",
+        "inpkg_LRBRL": "0",
+        "inpkg_LRMAC": "0",
+        "inpkg_LRMC": "0",
+        "inpkg_LRMP": "0",
+        "inpkg_LRTB": "0",
+        "inpkg_PHLT": "0",
+        "inpkg_PHLT2": "0",
+        "inpkg_PLES": "0",
+        "storage_quota": "100"
+      }
+    };
+obj.entitlement.storage={
+      "used": 0,
+      "limit": 112742891520,
+      "display_limit": 107374182400,
+      "warn": 85899345920
+    };
+obj.avatar.placeholder=true;
+body = JSON.stringify(obj); 
+$done({body});
